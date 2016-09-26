7c8490e797b5eb40030ea976cc85420d74d1548e
Lucas Viecelli <lviecelli199@gmail.com>
2016-09-26 15:47:12 -0300

    FInalizado ajustes

--- app/models/config.rb
+++ app/models/config.rb
@@ -1,22 +1,15 @@
-class Config < ActiveRecord::Base
-#  validates :type, :step_old, :step_new, presence: true
-
   ATTR_ISSUE = ['status_id', 'fixed_version_id']
-  # The padron of the project is code in model, I'm think bad ideia.
+  class Config < ActiveRecord::Base
 
   def self.changes?(sinc_model)
     begin
-
       if sinc_model.class.table_name == 'issues' && sinc_model.changes.present?
         return unless sinc_model.changes.keys.find{|a| ATTR_ISSUE.include?(a)}.present?
         return if Config.find_by_config_name('not_sinc_issue_situation_id').value.split(',').include?(sinc_model.status_id)
-
         hash = prepare_json_issues(sinc_model.get_value_protocol.split(';'), sinc_model)
-      elsif sinc_model.class.table_name == 'custom_values' # para o protocolo
+      elsif sinc_model.class.table_name == 'custom_values'
         return if sinc_model.custom_field_id != Config.find_by_config_name('protocol_custom_field_id').value.to_i
         return unless sinc_model.changes.keys.find{|a| 'value' == a}.present?
-
-        # foi alterado o protocolo
         issue = Issue.find(sinc_model.customized_id)
         hash = prepare_json_issues(sinc_model.value.split(';'), issue)
       elsif sinc_model.class.table_name == 'versions'
@@ -47,7 +40,7 @@ class Config < ActiveRecord::Base
   # Alteração de data
 
   def self.change_effective_date_version(version)
-    setting = Setting.find_by_name('protocol_custom_field_id')
+    setting = Config.find_by_config_name('protocol_custom_field_id')
     issues_ids = Issue.where(fixed_version_id: version.id).select('id').map(&:id)
     protocols = CustomValue.where(customized_id: issues_ids, custom_field_id: setting.value).select('value').map{|v| v.value.split(';')}.flatten.uniq
 
@@ -84,7 +77,7 @@ class Config < ActiveRecord::Base
   end
 
   def self.ws(hash)
-    uri = URI(Setting.find_by_name("url_webservice")[:value])
+    uri = URI(Config.find_by_config_name("url_webservice").value)
 
     https = Net::HTTP.new(uri.host, uri.port)
     resp = https.post(uri.path, hash.to_json, {'Content-Type' => 'application/json'})
